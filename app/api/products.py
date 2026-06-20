a8ef5d0ea4e03661124c406d3051b0bd234d6e32
juan <liljuan4168@Juans-Mba.local>
2026-06-20 02:22:10 -0300

    feat - endpoints for products and users

--- app/api/products.py
+++ app/api/products.py
@@ -2,9 +2,22 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/all")
+@router.get("/product_by_id/{product_id}")
+def get_product_by_id(product_id: str):
+    return {"message": "List of products"}
+
+@router.get("/product_by_name/{product_name}")
+def get_product_by_name(product_name: str):
+    return {"message": "List of products"}
+
+@router.get("/product_all")
 def list_products():
     return {"message": "List of products"}
 
+@router.post("/add_product")
+def list_products():
+    return {"message": "List of products"}
 
-
+@router.get("/delete_product/{product_id}")
+def list_products():
+    return {"message": "List of products"}
--- app/api/users.py
+++ app/api/users.py
@@ -2,6 +2,18 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/all")
+@router.get("/user_all")
+def list_users():
+    return {"message": "List of users"}
+
+@router.post("/user_add")
+def add_user():
+    return {"message": "List of users"}
+
+@router.get("/user_loyalty/{user_id}")
+def get_user_loyalty(user_id: str):
+    return {"message": "List of users"}
+
+@router.post("/user_address/{user_id}")
 def list_users():
     return {"message": "List of users"}
